1ec82be0ea51d05bb784d486df9ae15cf1d012de
m3mange <52103683+m3mange@users.noreply.github.com>
2019-07-03 12:56:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function blueButton(){
+    mainBox.style.backgroundColor = "blue";
+    colorName.innerHTML = "Hi my name is Blue";
+    
+    var blueASCII = 66+108+117+101;
+    calculation.innerHTML = "ASCII Value is: " + blueASCII;
+}
+
+function redButton(){
+    mainBox.style.backgroundColor = "red";
+    colorName.innerHTML = "Hi my name is Red";
+    
+    var redASCII = 82+101+100;
+    calculation.innerHTML = "ASCII Value is: " + redASCII;
+}
+function greenButton(){
+    mainBox.style.backgroundColor = "green";
+    colorName.innerHTML = "Hi my name is Green";
+    
+    var greenASCII = 71+114+101+101+110;
+    calculation.innerHTML = "ASCII Value is: " + greenASCII;
+}
